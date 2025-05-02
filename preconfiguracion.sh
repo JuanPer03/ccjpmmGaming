4b5aa8509f9a683da0cb80aa48b3c7130d217791
JuanPer03 <54423936+JuanPer03@users.noreply.github.com>
2025-05-02 17:44:40 -0600

    Update preconfiguracion.sh
    Se agrega la parte de autoarranque de la consola

--- preconfiguracion.sh
+++ preconfiguracion.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Script de preconfiguracion para RetroConsole en Raspberry Pi OS Lite
+# Script de instalacion para RetroConsole en Raspberry Pi OS Lite
 # Muestra mensajes de progreso durante la instalación
 # Autor: ccjpmmGaming 
 echo ""
 echo ""
 echo "==================================================="
-echo "  PRECONFIGURACIÓN PARA RETROCONSOLE EN RASPBERRY  "
+echo "  INSTALACIÓN PARA RETROCONSOLE EN RASPBERRY  "
 echo "==================================================="
 echo ""
 echo ""
@@ -68,7 +68,33 @@ echo "==================================================="
 echo "Copiando archivos de consola..."
 unzip ccjpmmGaming/Retroconsole.zip -d /home/ccjpmmGaming
 
-# 9. Copiar configuracion inicial de mednafen
+# 9. Hacer que el programa de la consola se ejecute al encender
+echo ""
+echo ""
+echo "==================================================="
+echo "Aplicado la configuracion de arranque automatico...."
+# Ruta del script de Python que se va a ejecutar al inicio
+PYTHON_SCRIPT="/home/ccjpmmGaming/Retroconsole/code/Code.py"
+# Ruta del archivo de log (se creará automáticamente si no existe)
+LOG_FILE="/home/ccjpmmGaming/log.txt"
+# Verifica si el script de Python existe
+if [ ! -f "$PYTHON_SCRIPT" ]; then
+    echo "Error: El archivo $PYTHON_SCRIPT no existe."
+    exit 1
+fi
+# Crea el archivo log.txt si no existe
+touch "$LOG_FILE"
+# Verifica si ya existe una entrada @reboot en crontab
+if crontab -l | grep -q "@reboot.*$PYTHON_SCRIPT"; then
+    echo "Cron ya está configurado para ejecutar $PYTHON_SCRIPT al inicio."
+else
+    # Agrega la línea @reboot a crontab
+    (crontab -l 2>/dev/null; echo "@reboot python3 $PYTHON_SCRIPT >> $LOG_FILE 2>&1") | crontab -
+    echo "Configuración de cron agregada para ejecutar $PYTHON_SCRIPT al inicio."
+fi
+echo "¡Listo! configuracion de autoarranque hecha"
+
+# 10. Copiar configuracion inicial de mednafen
 echo ""
 echo ""
 echo "==================================================="
@@ -85,9 +111,9 @@ sleep 10
 kill $MEDNAFEN_PID
 cp ccjpmmGaming/mednafen.cfg ~/.mednafen/mednafen.cfg
 
-# 10. Mostrar mensaje de salida
+# 11. Mostrar mensaje de salida
 echo "================================================="
-echo " PRECONFIGURACIÓN COMPLETADA SATISFACTORIAMENTE! "
+echo " INSTALACIÓN COMPLETADA SATISFACTORIAMENTE! "
 echo "================================================="
 echo ""
 echo "El sistema se reiniciará en 10 segundos..."
